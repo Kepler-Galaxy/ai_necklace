0e2831923b585b62d811c040af6bf3e536e8ff03
zhihuangliu <zhihuangliu@deeproute.ai>
2024-08-28 01:05:03 +0800

    resolve conflict

--- backend/utils/other/endpoints.py
+++ backend/utils/other/endpoints.py
@@ -8,7 +8,7 @@ from firebase_admin import auth
 from firebase_admin.auth import InvalidIdTokenError
 
 
-def get_current_user_uid(authorization: str = Header(None)):
+def get_current_user_uid(authorization: str = Header(None), provider: str = Header(None)):
     if os.getenv('ADMIN_KEY') in authorization:
         return authorization.split(os.getenv('ADMIN_KEY'))[1]
 
@@ -19,9 +19,14 @@ def get_current_user_uid(authorization: str = Header(None)):
 
     try:
         token = authorization.split(' ')[1]
-        decoded_token = auth.verify_id_token(token)
-        print('get_current_user_uid', decoded_token['uid'])
-        return decoded_token['uid']
+        # 如果使用的authing
+
+        if provider == 'authing':
+            pass
+        else:
+            decoded_token = auth.verify_id_token(token)
+            print('get_current_user_uid', decoded_token['uid'])
+            return decoded_token['uid']
     except InvalidIdTokenError as e:
         if os.getenv('LOCAL_DEVELOPMENT') == 'true':
             return '123'
